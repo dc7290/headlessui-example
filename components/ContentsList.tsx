7967135de14f146cbb564840c4350daa15695c1d
dc7290 <dhkh.cba0927@gmail.com>
2022-09-15 18:14:41 +0900

    仮のコンテンツリストを実装

---
+++ components/ContentsList.tsx
@@ -0,0 +1,27 @@
+import styles from './ContentsList.module.css';
+
+type Content = {
+  title: string;
+  description: string;
+  createdAt: string;
+};
+
+type Props = {
+  list: Content[];
+};
+
+const ContentsList: React.FC<Props> = ({ list }) => {
+  return (
+    <ul className={styles.list}>
+      {list.map((item) => (
+        <li key={item.title} className={styles.item}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          <time>{item.createdAt}</time>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export { ContentsList };
